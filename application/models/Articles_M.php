01a307d37c094dbef5cf250cbfb671f9415f37aa
nickhargreaves <hargreaves1234@gmail.com>
2014-11-29 17:46:05 +0300

    filter by time bounds

--- application/models/Articles_M.php
+++ application/models/Articles_M.php
@@ -4,10 +4,35 @@ class Articles_M extends CI_Model {
 
     public function get_articles($start, $end, $location){
 
-        $articles = array();
+        //db query to retrieve articles
+        $this->db->select("*");
+        $this->db->from('stories');
+        $result = $this->db->get();
+        $all_stories = $result->result_array();
 
+        //filter articles by time bounds
+        $articles = $this->filter_by_time($all_stories, $start, $end);
+
+        //return json response
         return json_encode($articles);
 
     }
+
+    public function filter_by_time($all_stories, $start, $end){
+
+        $articles = array();
+
+        foreach($all_stories as $story){
+
+            $pubDate = strtotime($story['pubDate']);
+
+            if(($pubDate>=$start)&&($pubDate<=$end)){
+                $articles[] = $story;
+            }
+        }
+
+        return $articles;
+    }
+
 
 }
