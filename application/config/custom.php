42b41177b49b71d7b05ed93fb5e553bcf3d80f2e
BrunoChauvet <bruno.chauvet@live.fr>
2014-11-30 19:43:31 +1100

    Key!

--- application/config/custom.php
+++ application/config/custom.php
@@ -1,3 +1,3 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['google_geocode_api_key']	= 'AIzaSyC2pDpNpBWnlNYnBUX363XV5Aog4UdOjeg';
+$config['google_geocode_api_key']	= '';
