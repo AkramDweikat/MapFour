cc836227320eaac824df1af41ab29caf0204a706
BrunoChauvet <bruno.chauvet@live.fr>
2014-11-30 17:08:13 +1100

    Fetch stories controller update

--- application/controllers/stories.php
+++ application/controllers/stories.php
@@ -3,18 +3,21 @@
 class Stories extends CI_Controller {
 	public function index() {
 		$pagenumber = 1;
-		for ($pagenumber = 0; $pagenumber <= 10; $pagenumber++) {
-			$response = \Httpful\Request::get('http://devapi.aljazeera.com/v1/en/stories/latest?format=json&apikey=UBsPSKqownYvsDaE9l96Jq1aAOQOHXgF&pagenumber=' . $pagenumber)->send();
+		for ($pagenumber = 0; $pagenumber <= 1000; $pagenumber++) {
+			try {
+				$response = \Httpful\Request::get('http://devapi.aljazeera.com/v1/en/stories/latest?format=json&apikey=UBsPSKqownYvsDaE9l96Jq1aAOQOHXgF&pagenumber=' . $pagenumber)->send();
 
-			foreach ($response->body->stories as $story) {
-
-				$this->load->model('Stories_M');
-	      $record = $this->Stories_M->findByGuid($story->guid);
-				if ($record) {
-					$this->Stories_M->update($story);
-				} else {
-					$this->Stories_M->insert($story);
+				foreach ($response->body->stories as $story) {
+					$this->load->model('Stories_M');
+		      $record = $this->Stories_M->findByGuid($story->guid);
+					if ($record) {
+						$this->Stories_M->update($story);
+					} else {
+						$this->Stories_M->insert($story);
+					}
 				}
+			} catch (Exception $e) {
+			  echo 'Caught exception: ',  $e->getMessage(), "\n";
 			}
 		}
 
